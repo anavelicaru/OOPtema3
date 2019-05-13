8a65d5745a73570168e0b6253ef3e91994a41b7f
Ana Velicaru <ana.velicaru@yahoo.com>
2019-05-14 01:12:13 +0300

    Update client.h

--- client.h
+++ client.h
@@ -1,16 +1,21 @@
 #ifndef CLIENT_H_INCLUDED
 #define CLIENT_H_INCLUDED
 #include <vector>
+#include "haina.h"
 class Client{
+    //hainele pe care mi le a adus si CNP-ul
+    //vector de haine curate si haine murdare
 public:
     Client();
     Client(vector <Haina*> h, int nHaine);//haina & numarul de itemi
     Client(const Client&);
     ~Client();
+    int getidClient(int id);
 //friend istream& operator >> (istream&, Client&);
 protected:
     vector <Haina*>* comanda;
     int nHaine;
+    int idClient;
 };
 
 #endif // CLIENT_H_INCLUDED
